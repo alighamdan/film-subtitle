ac6b87c5939ca3cc9851ad2a7c3bec9c52f136f2
alighamdan <aligame9070@gmail.com>
2022-01-08 21:30:08 +0300

    prettie code.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var __awaiter =
       return value instanceof P
         ? value
         : new P(function (resolve) {
-            resolve(value);
-          });
+          resolve(value);
+        });
     }
     return new (P || (P = Promise))(function (resolve, reject) {
       function fulfilled(value) {
@@ -36,14 +36,14 @@ var __generator =
   (this && this.__generator) ||
   function (thisArg, body) {
     var _ = {
-        label: 0,
-        sent: function () {
-          if (t[0] & 1) throw t[1];
-          return t[1];
-        },
-        trys: [],
-        ops: [],
+      label: 0,
+      sent: function () {
+        if (t[0] & 1) throw t[1];
+        return t[1];
       },
+      trys: [],
+      ops: [],
+    },
       f,
       y,
       t,
@@ -51,9 +51,9 @@ var __generator =
     return (
       (g = { next: verb(0), throw: verb(1), return: verb(2) }),
       typeof Symbol === "function" &&
-        (g[Symbol.iterator] = function () {
-          return this;
-        }),
+      (g[Symbol.iterator] = function () {
+        return this;
+      }),
       g
     );
     function verb(n) {
@@ -67,13 +67,13 @@ var __generator =
         try {
           if (
             ((f = 1),
-            y &&
+              y &&
               (t =
                 op[0] & 2
                   ? y["return"]
                   : op[0]
-                  ? y["throw"] || ((t = y["return"]) && t.call(y), 0)
-                  : y.next) &&
+                    ? y["throw"] || ((t = y["return"]) && t.call(y), 0)
+                    : y.next) &&
               !(t = t.call(y, op[1])).done)
           )
             return t;
@@ -148,7 +148,7 @@ import child_process_1 from "child_process";
 const figlet_1 = figlet;
 
 var FilmSubtitle = /** @class */ (function () {
-  function FilmSubtitle() {}
+  function FilmSubtitle() { }
   FilmSubtitle.prototype.suggestions = function (filmName) {
     return __awaiter(this, void 0, void 0, function () {
       return __generator(this, function (_a) {
@@ -158,7 +158,7 @@ var FilmSubtitle = /** @class */ (function () {
               4 /*yield*/,
               axios_1.get(
                 "https://isubtitles.org/ajax/suggestion?query=" +
-                  filmName.replace(/\s/g, "+")
+                filmName.replace(/\s/g, "+")
               ),
             ];
           case 1:
@@ -274,9 +274,9 @@ function toArray(nodelist) {
             if (supportsColors) {
               process.stdout.write(
                 "\r" +
-                  chalk_1.bold.yellow(
-                    "Please Wait! Checking Wifi Connection" + points
-                  )
+                chalk_1.bold.yellow(
+                  "Please Wait! Checking Wifi Connection" + points
+                )
               );
             } else {
               process.stdout.write(
@@ -343,13 +343,11 @@ function toArray(nodelist) {
             " Film\n\n" +
             films
               .map(function (e) {
-                return `# ${e.filmName}\n## subtitles:\n ${e.subtitles.filter(e=>e.size !== 0).map(
-                  (s,index) => {
-                    return `### ${index+1}- Movie/Release: ${
-                      s.movie_release
-                    }\n### Language: ${s.language}\n${
-                      s.comment ? `Comment: ${s.comment}\n` : ""
-                    }### Translate File Size: ${s.size}KB\n### CreatedAt: ${s.createdAt}\n### [Download Translate File](${s.downloadUrl})`;
+                return `# ${e.filmName}\n## subtitles:\n ${e.subtitles.filter(e => e.size !== 0).map(
+                  (s, index) => {
+                    return `### ${index + 1}- Movie/Release: ${s.movie_release
+                      }\n### Language: ${s.language}\n${s.comment ? `Comment: ${s.comment}\n` : ""
+                      }### Translate File Size: ${s.size}KB\n### CreatedAt: ${s.createdAt}\n### [Download Translate File](${s.downloadUrl})`;
                   }
                 ).join('\n<br />\n')}`;
               })
